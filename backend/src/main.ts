f6ba63e2c096c879b08a605a53077a04649ec9fb
leandro <0xleanlp@gmail.com>
2023-05-02 07:54:25 -0300

    swagger

---
+++ backend/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('cryptoMark3t')
+    .setDescription('cryptoMark3t')
+    .setVersion('1.0')
+    .addTag('cryptoMark3t')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
