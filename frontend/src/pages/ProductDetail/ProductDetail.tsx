23144c498630350c843da96cf48b8d7424da39f0
NarcisaColcha <narcisacolcha@outlook.es>
2023-05-28 18:07:29 -0500

    [ADD] Product Detail - complete

--- frontend/src/pages/ProductDetail/ProductDetail.tsx
+++ frontend/src/pages/ProductDetail/ProductDetail.tsx
@@ -16,6 +16,7 @@ const ProductDetail: React.FC = () => {
 		variables: { id },
 	});
 	const adaptProducts = (data: Product) => {
+		console.log(data);
 		setProduct(data);
 	};
 
@@ -32,12 +33,39 @@ const ProductDetail: React.FC = () => {
 	if (!product) return <h1>Loading...</h1>;
 
 	return (
-		<div className='px-6 xl:container mx-auto mb-10'>
-			<h1>ProductDetail</h1>
-			<img src={product.image} alt={product.name} />
-			<p>
-				{product.name} ${product.price}
-			</p>
+		<div className='px-6 xl:container mx-auto mb-10' style={{ width: '100%' }}>
+			<br />
+			<h1>Product Detail:</h1>
+			<br />
+			<br />
+			<figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+				<div className="pt-6 md:p-8">
+					<img className='rounded float-start' src={product.image} alt={product.name} />
+				</div>
+				<div className="pt-6 md:p-8 text-center md:text-left space-y-4" style={{ position: 'relative' }}>
+					<blockquote>
+						<h1 className='text-black'>
+							{product.name}
+						</h1>
+						<hr style={{ border: '0.01px solid #2a5880'}}></hr>
+						<br />
+						<div className="md: p-2">
+							<p className='text-black'> 
+								{product.description}
+							</p>
+						</div>
+					</blockquote>
+					<br />
+					<br />
+					<figcaption className="font-medium" style={{ position: 'absolute', bottom: '2em', width: '100%'}}>
+						<hr style={{ border: '0.01px solid #2a5880', marginRight: '4em' }}></hr>				
+						<div className="text-sky-501 dark:text-sky-500 text-2xl font-semibold">
+							Price: ${product.price}
+						</div>
+					</figcaption>
+				</div>
+			</figure>
+
 		</div>
 	);
 };
